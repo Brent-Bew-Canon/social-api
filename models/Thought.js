623b08a2efd6b1105080b676b65292fe53037da1
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-07-05 11:25:23 -0700

    Created reaction and thought models

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create Student model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 128
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // TODO: Use a getter method to format the timestamp on query
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [
+      // TODO: Array of nested documents created with the reactionSchema
+      reactionSchema
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length
+  })
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
